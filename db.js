1d6c7f704d91700ebfbe01c3e9ea503e5c3339e4
CodeWithMufaddal <CodeWithMufaddal@gmail.com>
2022-02-22 19:42:22 +0530

    fix some issues

--- db.js
+++ db.js
@@ -3,7 +3,6 @@ require('dotenv').config({ path: './.env.local', });
 const mongoURI = process.env.MONGODBURI
 
 
-
 const ConnectToMongo = async () => {
    await mongoose.connect(mongoURI)
    console.log("Connected to MongoDB")  
